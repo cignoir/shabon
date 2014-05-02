129153cbac2a9df98602896f5e11c3c957d8f4d5
cignoir <cignoir@gmail.com>
2014-05-02 15:46:01 +0900

    first commit

---
+++ Assets/Scripts/Screen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screen : MonoBehaviour {
+
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length > 0) {
+			var webcamTexture = new WebCamTexture(devices[0].name ,320, 240, 10);
+			renderer.material.mainTexture = webcamTexture;
+			webcamTexture.Play();
+		}
+	}
+	
+	void Update () {
+	}
+}
